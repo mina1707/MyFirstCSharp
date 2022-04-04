2e2d60ce2a4b098d95ecb9b9b62ca27522580202
Xiomina Ramos <98005220+mina1707@users.noreply.github.com>
2022-04-04 15:18:11 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string place = "Coding Dojo";
+            string message = $"Hola {place}";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hello {place}");
+            Console.WriteLine(message);
+        }
+    }
+}
